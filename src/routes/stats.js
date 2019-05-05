83fa5f5713cb39add7a30ac71512d546b70b5bc6
Nienke Dekker <nienkedekker@gmail.com>
2019-05-05 16:37:27 +0200

    optimize api for charts

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -3,71 +3,59 @@ const router = express.Router();
 const Book = require('../models/Book.js');
 const Show = require('../models/Show.js');
 const Movie = require('../models/Movie.js');
+const async = require('async');
 
 /**
- * GET amount of items ever logged
- * ex: host.com/api/stats/{item}
- * returns count of items from every year
- */
-const getAllTimeCount = (model, req, res, next) => {
-  model.countDocuments((err, count) => {
+* GET amount of items logged by year
+* ex: host.com/api/stats/count/:year/{item}
+* returns count of items from every year
+*/
+const getCountOfAllItemsByYear = (req, res, next) => {
+  async.parallel({
+    movies: cb => Movie.countDocuments({'belongs_to_year': req.params.year }, cb),
+    books: cb => Book.countDocuments({'belongs_to_year': req.params.year }, cb),
+    shows: cb => Show.countDocuments({'belongs_to_year': req.params.year }, cb),
+  }, (err, items) => {
     if (err) return next(err);
-    res.json(count);
+    res.json({
+      books: items.books,
+      movies: items.movies,
+      shows: items.shows,
+    });
   });
 };
 
 /**
- * GET amount of items logged by year
- * ex: host.com/api/stats/count/:year/{item}
- * returns count of items from every year
+ * GET amount of items of all time
+ * ex: host.com/api/stats/count/all
  */
-const getCountByYear = (model, req, res, next) => {
-  model.countDocuments({'belongs_to_year': req.params.year }, (err, itemCount) => {
+const getAllTimeCount = (req, res, next) => {
+  async.parallel({
+    books: cb => Book.countDocuments({}, cb),
+    movies: cb => Movie.countDocuments({}, cb),
+    shows: cb => Show.countDocuments({}, cb),
+  }, (err, items) => {
     if (err) return next(err);
-    res.json(itemCount);
+    res.json({
+      books: items.books,
+      movies: items.movies,
+      shows: items.shows,
+    });
   });
 };
 
 /**
- * host.com/api/stats/count/:year/movies
+ * host.com/api/stats/count/all
  */
-router.get('/count/:year/movies', (req, res, next) => {
-  getCountByYear(Movie, req, res, next);
+router.get('/count/all', (req, res, next) => {
+  getAllTimeCount(req, res, next);
 });
 
 /**
- * host.com/api/stats/count/:year/books
+ * host.com/api/stats/count/:year/all
  */
-router.get('/count/:year/books', (req, res, next) => {
-  getCountByYear(Book, req, res, next);
-});
-
-/**
- * host.com/api/stats/count/:year/shows
- */
-router.get('/count/:year/shows', (req, res, next) => {
-  getCountByYear(Show, req, res, next);
-});
-
-/**
- * host.com/api/stats/count/books
- */
-router.get('/count/books', (res, next) => {
-  getAllTimeCount(Book, res, next);
-});
-
-/**
- * host.com/api/stats/count/shows
- */
-router.get('/count/shows', (res, next) => {
-  getAllTimeCount(Show, res, next);
-});
-
-/**
- * host.com/api/stats/count/movies
- */
-router.get('/count/movies', (res, next) => {
-  getAllTimeCount(Movie, res, next);
+router.get('/count/:year/all', (req, res, next) => {
+  getCountOfAllItemsByYear(req, res, next);
 });
 
 module.exports = router;
