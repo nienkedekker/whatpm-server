9fb204b42a53850d5d377491c94291e2d59cf0a8
Nienke Dekker <nienke.dekker@nos.nl>
2020-10-04 19:17:10 +0200

    add item route in preparation for move to nextjs

--- src/app.js
+++ src/app.js
@@ -1,45 +1,52 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import books from './routes/books';
-import movies from './routes/movies';
-import shows from './routes/shows';
-import years from './routes/years';
-import authentication from './routes/authentication';
-import search from './routes/search';
-import stats from './routes/stats';
+import books from "./routes/books";
+import item from "./routes/item";
+import movies from "./routes/movies";
+import shows from "./routes/shows";
+import years from "./routes/years";
+import authentication from "./routes/authentication";
+import search from "./routes/search";
+import stats from "./routes/stats";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-const corsOriginDevelopment = 'http://localhost:8080';
-const corsOriginProduction = 'https://what.pm';
+const corsOriginDevelopment = "http://localhost:8080";
+const corsOriginProduction = "https://what.pm";
 
-app.use(cors({
-  credentials: true,
-  origin: process.env.NODE_ENV === 'production' ? corsOriginProduction : corsOriginDevelopment,
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin:
+      process.env.NODE_ENV === "production"
+        ? corsOriginProduction
+        : corsOriginDevelopment,
+  })
+);
 
 const mongoDbUrl = process.env.MONGO_DB_URL;
-mongoose.Promise = require('bluebird');
+mongoose.Promise = require("bluebird");
 
 mongoose
-  .connect(
-    mongoDbUrl,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      // eslint-disable-next-line global-require
-      promiseLibrary: require('bluebird'),
-    },
+  .connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    // eslint-disable-next-line global-require
+    promiseLibrary: require("bluebird"),
+  })
+  .then(() =>
+    console.log(
+      ` → Successful connection to MongoDB URL: ${process.env.MONGO_DB_URL}`
+    )
   )
-  .then(() => console.log(` → Successful connection to MongoDB URL: ${process.env.MONGO_DB_URL}`))
   .catch((err) => console.error(err));
 
 app.use(bodyParser.json());
@@ -47,16 +54,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set up routes
-app.use('/api/authentication', authentication);
-app.use('/api/books', books);
-app.use('/api/movies', movies);
-app.use('/api/shows', shows);
-app.use('/api/years', years);
-app.use('/api/search', search);
-app.use('/api/stats', stats);
+app.use("/api/authentication", authentication);
+app.use("/api/books", books);
+app.use("/api/movies", movies);
+app.use("/api/shows", shows);
+app.use("/api/years", years);
+app.use("/api/search", search);
+app.use("/api/stats", stats);
+app.use("/api/item", item);
 
 // When someone accesses the API directly via browser, don't show an error
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.end();
 });
 
---
+++ src/routes/item.js
@@ -0,0 +1,24 @@
+// TODO: fix linting errors
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+
+import express from "express";
+import passport from "passport";
+import Item from "../models/Item";
+
+require("../authentication/passport")(passport);
+
+const router = express.Router();
+
+/**
+ * GET a single item by ID
+ * ex: host.com/api/item/123456
+ */
+router.get("/:id", (req, res, next) => {
+  Item.findById(req.params.id, (err, item) => {
+    if (err) return next(err);
+    res.json(item);
+  });
+});
+
+export default router;
